89cc159b59db6593a90af9c79852a236712f86c7
Daniel Fraga <dan.fraga33@gmail.com>
2024-07-05 16:00:42 +0100

    feat: add gsap animation with sphere

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+import * as THREE from 'three';
+import './style.css';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap';
+
 const scene = new THREE.Scene();
 // Create sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
@@ -57,3 +62,32 @@ const loop = () => {
 	window.requestAnimationFrame(loop);
 };
 loop();
+
+// Timeline
+const tl = gsap.timeline({ default: { duration: 1 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo('nav', { y: '-100%' }, { y: '0%' });
+tl.fromTo('title', { opacity: 0 }, { opacity: 1 });
+
+// Mouse animation Color
+let mouseDown = false;
+let rgb = [];
+window.addEventListener('mousedown', () => (mouseDown = true));
+window.addEventListener('mouseup', () => (mouseDown = false));
+
+window.addEventListener('mousemove', (e) => {
+	if (mouseDown) {
+		rgp = [
+			Math.round((e.pageX / sizes.width) * 255),
+			Math.round((e.pageY / sizes.width) * 255),
+			150,
+		];
+		// Animate
+		let newColor = new THREE.Color(`rgb${rgb.join(',')}`);
+		gsap.to(mesh.material.color, {
+			r: newColor.r,
+			g: newColor.g,
+			b: newColor.b,
+		});
+	}
+});
